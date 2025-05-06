267c5c4e196c9a7ba9ecdad188da818ba19bb12c
iknowozey <wooxzee@gmail.com>
2025-05-07 06:51:05 +0700

    ui: header changed

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,7 +4,7 @@ import React from "react"
 import Image from "next/image"
 import { Button, Input } from "../ui"
 import { ArrowRight, ShoppingBasket } from "lucide-react"
-import { ModeToggle } from "./mode-toggle"
+import { ToggleTheme } from "./toggle-theme"
 
 interface Props {
 	className?: string
@@ -19,46 +19,49 @@ export const Header: React.FC<Props> = ({ className }) => {
 				className
 			)}
 		>
-			<Link href="/">
-				<Image
-					className="block dark:hidden"
-					src="/logo-dark.svg"
-					alt="Логотип (светлая тема)"
-					width={110}
-					height={22}
-				/>
-				<Image
-					className="hidden dark:block"
-					src="/logo-light.svg"
-					alt="Логотип (темная тема)"
-					width={110}
-					height={22}
-				/>
-			</Link>
-			<Link href="/about">
-				<p className="text-xs">О нас</p>
-			</Link>
-			<Link href="/contacts">
-				<p className="text-xs">Контакты</p>
-			</Link>
-			<Input placeholder="Поиск" className="w-164.5" />
-			<Link href="https://github.com/iknowozey">
-				<p className="text-xs">GitHub</p>
-			</Link>
-			<Button variant="outline">Войти</Button>
-			<Button className={cn("group relative", className)} variant='outline'>
-				<b className="text-xs font-normal">0 ₽</b>
-				<span className="h-full w-[1px] bg-primary mx-1" />
-				<div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-					<ShoppingBasket className="relative" strokeWidth={1} />
-					<b className="text-xs font-normal">0</b>
+			<div className="flex items-center gap-10">
+				<Link href="/">
+					<Image
+						className="block dark:hidden"
+						src="/logo-dark.svg"
+						alt="Логотип"
+						width={110}
+						height={22}
+					/>
+					<Image
+						className="hidden dark:block"
+						src="/logo-light.svg"
+						alt="Логотип"
+						width={110}
+						height={22}
+					/>
+				</Link>
+				<Link href="/about">
+					<p className="text-xs">О нас</p>
+				</Link>
+				<Link href="https://github.com/iknowozey">
+					<p className="text-xs">GitHub</p>
+				</Link>
+			</div>
+			<Input placeholder="Поиск" className="w-200" />
+			<div className="flex items-center gap-10">
+				<div className="flex gap-5">
+					<Button variant="outline">Войти</Button>
+					<Button className={cn("group relative", className)} variant="outline">
+						<b className="text-xs font-normal">0 ₽</b>
+						<span className="h-full w-[1px] bg-primary mx-1" />
+						<div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+							<ShoppingBasket className="relative" strokeWidth={1} />
+							<b className="text-xs font-normal">0</b>
+						</div>
+						<ArrowRight
+							size={20}
+							className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+						/>
+					</Button>
 				</div>
-				<ArrowRight
-					size={20}
-					className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
-				/>
-			</Button>
-			<ModeToggle />
+				<ToggleTheme />
+			</div>
 		</header>
 	)
 }
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,2 @@
 export { Button } from "./button"
-export { DropdownMenu } from "./dropdown-menu"
 export { Input } from "./input"
