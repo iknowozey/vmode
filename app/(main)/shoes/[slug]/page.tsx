b228e6900fa736cc818538d46c35f36024fa444c
iknowozey <wooxzee@gmail.com>
2025-05-12 15:54:18 +0700

    feat: added full information about selected sneakers

--- app/(main)/shoes/[slug]/page.tsx
+++ app/(main)/shoes/[slug]/page.tsx
@@ -1,9 +1,9 @@
-import { Container } from "@/components/shared"
+import { Container, DescriptionShoes, WarrantyInfo } from "@/components/shared"
 import prisma from "@/lib/prisma"
 import { notFound } from "next/navigation"
 import { BreadCrumb } from "@/components/shared"
 import Image from "next/image"
-import { BadgeCheck, ChevronDown, CircleHelp } from "lucide-react"
+import { ChevronDown } from "lucide-react"
 import { Button } from "@/components/ui"
 
 export default async function ShoesPage({
@@ -22,9 +22,9 @@ export default async function ShoesPage({
 	}
 
 	return (
-		<Container className="pt-5">
+		<Container className="px-40 pt-5">
 			<div className="flex flex-col items-center">
-				<div className="flex flex-col items-start">
+				<div className="flex flex-col">
 					<BreadCrumb shoes={shoes} />
 					<div className="flex gap-10 pt-4">
 						<div className="relative w-162.5 h-118 overflow-hidden rounded-lg">
@@ -36,76 +36,67 @@ export default async function ShoesPage({
 								alt={shoes.slug}
 							/>
 						</div>
-						<div>
-							<div className="flex items-center justify-between mb-4">
-								<p className="text-xl font-bold">{shoes.name}</p>
-								<p className="text-foreground/50 text-xl font-bold">
-									{shoes.color}
-								</p>
-							</div>
-							<Button
-								className="flex items-center justify-between px-4 border border-foreground w-full h-12 rounded-lg mb-4 font-normal"
-								variant="outline"
-							>
-								<p className="text-sm">Размер</p>
-								<div className="flex items-center gap-1">
-									<p>RUS 38</p>
-									<ChevronDown size={16} />
-								</div>
-							</Button>
-							<div className="flex items-center justify-between mb-6">
-								<p className="text-xl">{shoes.price} ₽</p>
-								<Button className="w-1/2 h-10">Добавить в корзину</Button>
-							</div>
-							<div className="flex justify-between gap-5 mb-5">
-								<div className="relative w-40 h-30 overflow-hidden rounded-lg">
-									<Image
-										className="w-full h-full"
-										src={shoes.imagesUrl[0]}
-										loading="lazy"
-										fill
-										alt={shoes.slug}
-									/>
-								</div>
-								<div className="relative w-40 h-30 overflow-hidden rounded-lg">
-									<Image
-										className="w-full h-full"
-										src={shoes.imagesUrl[1]}
-										loading="lazy"
-										fill
-										alt={shoes.slug}
-									/>
-								</div>
-								<div className="relative w-40 h-30 overflow-hidden rounded-lg">
-									<Image
-										className="w-full h-full"
-										src={shoes.imagesUrl[2]}
-										loading="lazy"
-										fill
-										alt={shoes.slug}
-									/>
-								</div>
-							</div>
-							<div className="flex flex-col gap-4">
-								<div className="flex items-center gap-2">
-									<CircleHelp size={14} />
-									<p className="max-w-120 leading-none text-sm">
-										Мы отвечаем за каждую проданную модель на vmode. Если мы
-										допустили ошибку - мы ее исправим.
+						<div className="flex flex-col justify-around bg-accent/80 p-6 rounded-lg">
+							<div>
+								<div className="flex items-center justify-between mb-4">
+									<p className="text-xl font-bold">{shoes.name}</p>
+									<p className="text-foreground/50 text-xl font-bold">
+										{shoes.color}
 									</p>
 								</div>
-								<div className="flex items-center gap-2">
-									<BadgeCheck size={14} />
-									<p className="max-w-120 leading-none text-sm">
-										Проверено. Наши сотрудники проверили товар и убедились в
-										подлинности. Вы с уверенностью можете приобрести его.
-									</p>
+								<Button
+									className="flex items-center justify-between px-4 border border-foreground w-full h-12 rounded-lg mb-4 font-normal"
+									variant="outline"
+								>
+									<p className="text-sm">Размер</p>
+									<div className="flex items-center gap-1">
+										<p>RUS 38</p>
+										<ChevronDown size={16} />
+									</div>
+								</Button>
+								<div className="flex items-center justify-between mb-8">
+									<p className="text-xl">{shoes.price} ₽</p>
+									<Button className="w-1/2 h-10">Добавить в корзину</Button>
+								</div>
+								<div className="flex justify-between mb-5">
+									<div className="relative w-40 h-30 overflow-hidden rounded-lg">
+										<Image
+											className="w-full h-full"
+											src={shoes.imagesUrl[0]}
+											loading="lazy"
+											fill
+											alt={shoes.slug}
+										/>
+									</div>
+									<div className="relative w-40 h-30 overflow-hidden rounded-lg">
+										<Image
+											className="w-full h-full"
+											src={shoes.imagesUrl[1]}
+											loading="lazy"
+											fill
+											alt={shoes.slug}
+										/>
+										<div className="absolute inset-0 bg-black opacity-70"></div>
+									</div>
+									<div className="relative w-40 h-30 overflow-hidden rounded-lg">
+										<Image
+											className="w-full h-full"
+											src={shoes.imagesUrl[2]}
+											loading="lazy"
+											fill
+											alt={shoes.slug}
+										/>
+										<div className="absolute inset-0 bg-black opacity-70"></div>
+									</div>
 								</div>
 							</div>
+							<WarrantyInfo />
 						</div>
 					</div>
 				</div>
 			</div>
+			<DescriptionShoes shoes={shoes} />
+			<p className="pt-40">Может быть интересно</p>
 		</Container>
 	)
 }
---
+++ components/shared/warranty-info.tsx
@@ -0,0 +1,25 @@
+import { CircleHelp, PackageCheck } from "lucide-react"
+import React from "react"
+
+export const WarrantyInfo: React.FC = () => {
+	return (
+		<>
+			<div className="flex flex-col gap-4 bg-foreground/5 text-foreground rounded-lg p-4">
+				<div className="flex items-center gap-2">
+					<CircleHelp size={20} />
+					<p className="max-w-120 leading-none text-xs">
+						Мы отвечаем за каждую проданную модель на vmode. Если мы допустили
+						ошибку - мы ее исправим.
+					</p>
+				</div>
+				<div className="flex items-center gap-2">
+					<PackageCheck size={24} />
+					<p className="max-w-120 leading-none text-xs">
+						Проверено. Наши сотрудники проверили товар и убедились в
+						подлинности. Вы с уверенностью можете приобрести его.
+					</p>
+				</div>
+			</div>
+		</>
+	)
+}
