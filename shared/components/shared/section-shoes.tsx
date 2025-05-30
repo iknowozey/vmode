a1df97a5f3a13c799d20485b406e4771ef242c0f
iknowozey <wooxzee@gmail.com>
2025-05-31 02:24:51 +0700

    refactor: logic of client filtering is moved to a hook

--- shared/components/shared/section-shoes.tsx
+++ shared/components/shared/section-shoes.tsx
@@ -4,7 +4,7 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
-import { useShoes } from "@/shared/hooks"
+import { useFilteredShoes, useShoes } from "@/shared/hooks"
 import { ScrollToTopWrapper } from "./scroll-to-top-wrapper"
 import { Skeleton } from "../ui"
 import { ArrowRight } from "lucide-react"
@@ -22,60 +22,7 @@ export const SectionShoes: React.FC<Props> = ({
 	appliedFilters,
 }) => {
 	const { shoes: allShoes, loading } = useShoes()
-
-	const filteredShoes = React.useMemo(() => {
-		let tempShoes = allShoes
-
-		const selectedBrands = Object.keys(appliedFilters?.brand || {}).filter(
-			key => appliedFilters?.brand[key]
-		)
-		if (selectedBrands.length > 0) {
-			tempShoes = tempShoes.filter(shoe =>
-				selectedBrands.includes(shoe.name.split(" ")[0].toLowerCase())
-			)
-		}
-
-		const selectedColors = Object.keys(appliedFilters?.color || {}).filter(
-			key => appliedFilters?.color[key]
-		)
-		if (selectedColors.length > 0) {
-			tempShoes = tempShoes.filter(
-				shoe => shoe.color && selectedColors.includes(shoe.color.toLowerCase())
-			)
-		}
-
-		const selectedSeasons = Object.keys(appliedFilters?.season || {}).filter(
-			key => appliedFilters?.season[key]
-		)
-		if (selectedSeasons.length > 0) {
-			tempShoes = tempShoes.filter(
-				shoe =>
-					shoe.season && selectedSeasons.includes(shoe.season.toLowerCase())
-			)
-		}
-
-		const selectedSexes = Object.keys(appliedFilters?.sex || {}).filter(
-			key => appliedFilters?.sex[key]
-		)
-		console.log("Selected sexes for filtering:", selectedSexes)
-		if (selectedSexes.length > 0) {
-			const initialCount = tempShoes.length
-			tempShoes = tempShoes.filter(
-				shoe => shoe.sex && selectedSexes.includes(shoe.sex.toLowerCase())
-			)
-			console.log(
-				`Filtered by sex. Before: ${initialCount}, After: ${tempShoes.length}`
-			)
-		}
-
-		const minPrice = appliedFilters?.price?.min ?? 0
-		const maxPrice = appliedFilters?.price?.max ?? 100000
-		tempShoes = tempShoes.filter(
-			shoe => shoe.price >= minPrice && shoe.price <= maxPrice
-		)
-
-		return tempShoes
-	}, [allShoes, appliedFilters])
+	const filteredShoes = useFilteredShoes(allShoes, appliedFilters)
 
 	return (
 		<>
